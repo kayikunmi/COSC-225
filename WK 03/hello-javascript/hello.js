24d19ad4130092d692a684f221cc54414474fc32
KayikunmiMac <kayikunmiba.com>
2023-02-13 15:12:12 -0500

    need better notes

--- WK 03/hello-javascript/hello.js
+++ WK 03/hello-javascript/hello.js
@@ -1,8 +1,9 @@
 function sayHello(n) {
     const helloElement = document.querySelector("#hello");
+    console.log("#hello");
     for (let i = 1; i <= n; i++) {
-	let para = document.createElement("p");
-	para.textContent = "Hello, World! " + i;
-	helloElement.appendChild(para);
+	    let para = document.createElement("p"); //crate paragraph elemt
+	    para.textContent = "Hello, World! " + i;
+	    helloElement.appendChild(para);
     }
 }
