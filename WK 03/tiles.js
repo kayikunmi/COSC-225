88091afda607a2bb4e541d8a697e90c286e03d3d
KayikunmiMac <kayikunmiba.com>
2023-02-15 17:27:57 -0500

    get started ish

--- WK 03/tiles.js
+++ WK 03/tiles.js
@@ -1,5 +1,5 @@
 function sayHello(n) {
-    const helloElement = document.querySelector("#hello");
+    const helloElement = document.querySelector("#grid"); //this is the name of the grid
     console.log("#hello");
     for (let i = 1; i <= n; i++) {
 	    let para = document.createElement("p"); //crate paragraph elemt
