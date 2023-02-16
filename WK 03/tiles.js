76b7d14f835cea015feee08be0ed71cc8ca1b82f
KayikunmiMac <kayikunmiba.com>
2023-02-16 18:27:28 -0500

    almost

--- WK 03/tiles.js
+++ WK 03/tiles.js
@@ -1,3 +1,6 @@
+// const red = Math.floor(Math.random() * 225) + 1;
+// const green = Math.floor(Math.random() * 225) + 1;
+// const blue = Math.floor(Math.random() * 225) + 1;
 function makeGrid(){
     const gridrows = 10;
     const gridcolumns= 10;
@@ -6,8 +9,9 @@ function makeGrid(){
         let row = document.createElement("tr");
         for (let j = 1; j <=gridcolumns; j++) {
             let cell = document.createElement("td");
-            cell.setAttribute("id", "cell" +i +"." +j)
-            let idname = cell.setAttribute("id", "cell" +i +"." +j)
+            cell.setAttribute("id", "r" + i + "c" + j)
+            let idname = cell.setAttribute("id", "r" + i + "c" + j)
+            cell.style.backgroundColor = "rgb(200,200,200)";
             row.appendChild(cell);
             console.log("name:" + idname);
          
