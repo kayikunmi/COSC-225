4792de85954708c54d86caf35f525e9962497929
KayikunmiMac <kayikunmiba.com>
2023-02-17 14:24:42 -0500

    ready to turn in

--- WK 03/tiles.js
+++ WK 03/tiles.js
@@ -1,22 +1,21 @@
-function makeGrid(){
+function drawGrid(){
     let color1 = 225;
     let color2 = 0;
     const gridrows = 10;
     const gridcolumns= 10;
-    const grid = document.getElementById("grid");
+    const table = document.getElementById("grid");
     for (let i = 1; i <= gridrows; i++) {
         let row = document.createElement("tr");
         for (let j = 1; j <=gridcolumns; j++) {
             let cell = document.createElement("td");
-            cell.setAttribute("id", "r" + i + "c" + j)
-            let idname = cell.setAttribute("id", "r" + i + "c" + j)
-            console.log("idname: " + idname);
+            cell.setAttribute("id", "r" + i + "c" + j);
+            let idname = cell.getAttribute('id')
+            //console.log("idname: " + idname);
             color1 -=2.25;
             color2 +=2.25;
             cell.style.backgroundColor = "rgb(230," + color2 + "," + color1;
             row.appendChild(cell);
-
         }
-        grid.appendChild(row);
+        table.appendChild(row);
     }
 }
