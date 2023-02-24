910f45b5d374cf016055458553b04a286b19d726
KayikunmiMac <kayikunmiba.com>
2023-02-24 09:04:20 -0500

    perfect

--- WK 04/HW4/cellular-automata.js
+++ WK 04/HW4/cellular-automata.js
@@ -1,80 +1,70 @@
-var decimal;
-var binary;
-var config = [];
-
-function getUserInput() {
-  var nameInput = document.getElementById('number');
-  if(nameInput != null){
-    document.querySelector('form.pure-form').addEventListener('submit', function (e) {
-        //prevent the normal submission of the form
-        e.preventDefault();
-        decimal = nameInput.value;
-        binary = Number(decimal).toString(2);
-        console.log("works here");
-        console.log("This is the input in decimal: " + decimal); 
-        console.log("This is the input in binary: " + binary);
-        
-        //this pads binary if necessary
-        while (binary.length < 8) {
-            binary = "0" + binary;
-            // console.log("here we are: " + binary);
-            // console.log(binary.length);
-        }
-
-        //clears the array if necessary
-        while(config.length > 0) {
-            config.pop();
-            //console.log("poping config: " + config.length);
-        }
-        
-        
-        // add the values of binary to the config array
-        for (let i = 7; i >= 0; i--){
-            config.push(binary[i]);
-        }
-    
-        console.log("The binary values in the config array are: " + config);
-        console.log("config length: " + config.length);
-    });
+// This function applies the cellular automata rule to the current configuration
+function applyRule(config, rule) {
+    const n = config.length;
+    const nextConfig = [];
+  
+    // Loop through the current configuration
+    for (let i = 0; i < n; i++) {
+      // Get the left, center and right cells
+      const left = config[(i + n - 1) % n];
+      const center = config[i];
+      const right = config[(i + 1) % n];
+  
+      // Calculate the sum of the left, center and right cells
+      const sum = left * 4 + center * 2 + right;
+  
+      // Apply the rule to the sum and update the next configuration
+      nextConfig[i] = (rule >> sum) & 1;
+    }
+  
+    // Return the next configuration
+    return nextConfig;
   }
   
-}
-
-// getUserInput();
-
-
-///
-
-function draw(){
-    let color1 = 225;
-    let color2 = 0;
-    const gridrows = 20;
-    const gridcolumns= 20;
-    const table = document.getElementById("grid");
-    for (let i = 1; i <= gridrows; i++) {
-        let row = document.createElement("tr");
-        for (let j = 1; j <=gridcolumns; j++) {
-            let cell = document.createElement("td");
-            cell.setAttribute("id", "r" + i + "c" + j);
-            let idname = cell.getAttribute('id')
-            //console.log("idname: " + idname);
-            color1 -=2.25;
-            color2 +=2.25;
-            //cell.style.backgroundColor = "rgb(230," + color2 + "," + color1 + ")";
-            if(j==i){
-                cell.style.backgroundColor = "rgb(230," + color2 + "," + color1 + ")";
-            }
-            else{
-                cell.style.backgroundColor = "rgb(255,255,255)";
-            }
-            row.appendChild(cell);
-        }
-        table.appendChild(row);
+  // Select the form and the grid from the HTML document
+  const form = document.querySelector('form');
+  const grid = document.querySelector('#grid');
+  
+  // Listen for form submit event
+  form.addEventListener('submit', (event) => {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+  
+    // Get the rule input value
+    const ruleInput = document.querySelector('#rule');
+    const rule = ruleInput.value;
+  
+    // Clear the grid before generating new cellular automaton
+    grid.innerHTML = '';
+  
+    // Define the number of rows and columns for the grid
+    const numRows = 150;
+    const numCols = 150;
+  
+    // Initialize the configuration with a single cell in the middle
+    let config = new Array(numCols).fill(0);
+    config[0]=1;
+    config[numCols-1] =1;
+    config[Math.floor(numCols / 2)] = 1;
+  
+    // Loop through each row and column and generate the cellular automaton
+    for (let i = 0; i < numRows; i++) {
+      const row = document.createElement('tr');
+  
+      for (let j = 0; j < numCols; j++) {
+        // Create a cell for the current row and column
+        const cell = document.createElement('td');
+  
+        // Set the class of the cell based on the configuration
+        cell.classList.toggle('alive', config[j] === 1);
+  
+        // Add the cell to the current row
+        row.appendChild(cell);
+      }
+  
+      // Apply the rule to the current configuration and update the grid
+      config = applyRule(config, rule);
+      grid.appendChild(row);
     }
-}
-///
-function applyRule(config, rule){
-    //here we want to apply whatever rule we've created
-}
-
- 
+  });
+  
