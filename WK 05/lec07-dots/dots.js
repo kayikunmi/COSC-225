21769d28e19bb6a61147b34eb82e1aab98f4e41a
KayikunmiMac <kayikunmiba.com>
2023-02-27 14:02:54 -0500

    wk 5 stuff

---
+++ WK 05/lec07-dots/dots.js
@@ -0,0 +1,11 @@
+const ns = "http://www.w3.org/2000/svg";
+
+const box = document.querySelector("#dot-box");
+box.addEventListener("click", drawDot);
+
+function drawDot(e) {
+    console.log("you drew a dot!");
+
+    /* complete this */
+
+}
