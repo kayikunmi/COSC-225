298e04fa0c7e08e4ba70284872cdda892ffb9e30
KayikunmiMac <kayikunmiba.com>
2023-03-05 10:44:47 -0500

    using mouseup/mousedown

--- WK 05/HW 05/draw.js
+++ WK 05/HW 05/draw.js
@@ -6,45 +6,38 @@ function drawLine(){
   svg.addEventListener('mousedown', startDrawing);
 
   function startDrawing(event){
-    // Get the position of the SVG element on the page
     let svgRect = svg.getBoundingClientRect();
 
-    // Set the start point of the line relative to the SVG element
+    // the start point has to be relative to the position of the box
     startPoint ={
       x: event.clientX - svgRect.left,
       y: event.clientY - svgRect.top
     };
 
-    // Create a new line element
     line = document.createElementNS(ns, 'line');
 
-    // Set the starting position of the line
+    // set the starting position of the line
     line.setAttribute('x1', startPoint.x);
     line.setAttribute('y1', startPoint.y);
-
-    // Set the line color and width
     line.setAttribute('stroke', 'green');
     line.setAttribute('stroke-width', '2');
 
-    // Add the line to the SVG element
     svg.appendChild(line);
-
     svg.addEventListener('mousemove', continueDrawing);
     svg.addEventListener('mouseup', stopDrawing);
     console.log("Called startDrawing");
   }
 
   function continueDrawing(event){
-    // Get the position of the SVG element on the page
     let svgRect = svg.getBoundingClientRect();
 
-    // Set the end point of the line relative to the SVG element
+    // the end point has to be relative to the position of the box
     endPoint ={
       x: event.clientX - svgRect.left,
       y: event.clientY - svgRect.top
     };
 
-    // Set the ending position of the line
+    // set the ending position of the line
     line.setAttribute('x2', endPoint.x);
     line.setAttribute('y2', endPoint.y);
     console.log("Called continueDrawing");
