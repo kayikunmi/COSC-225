f979513ecfcee5d541a0861bca37223c7f327254
KayikunmiMac <kayikunmiba.com>
2023-03-29 15:27:47 -0400

    kao's code

--- WK 07/hw06/convex-hull.js
+++ WK 07/hw06/convex-hull.js
@@ -2,43 +2,49 @@ const SVG_NS = "http://www.w3.org/2000/svg";
 const SVG_WIDTH = 600;
 const SVG_HEIGHT = 400;
 
+//global eventlistener for the button to call the step function
+let chb = document.querySelector("#convex-hull-box"); //my svg
+let set = new PointSet(); //my ps
+let chv = new ConvexHullViewer(chb, set); //the viewer containing svg and ps
+let inst = new ConvexHull(set, chv); //the instance that has ps and viewer
+
 // An object that represents a 2-d point, consisting of an
 // x-coordinate and a y-coordinate. The `compareTo` function
 // implements a comparison for sorting with respect to x-coordinates,
 // breaking ties by y-coordinate.
-function Point (x, y, id){
-    this.x = x;
-    this.y = y;
-    this.id = id;
+function Point(x, y, id) {
+  this.x = x;
+  this.y = y;
+  this.id = id;
 
-    // Compare this Point to another Point p for the purposes of
-    // sorting a collection of points. The comparison is according to
-    // lexicographical ordering. That is, (x, y) < (x', y') if (1) x <
-    // x' or (2) x == x' and y < y'.
-    this.compareTo = function (p){
-	if (this.x > p.x){
-	    return 1;
-	}
-
-	if (this.x < p.x){
-	    return -1;
-	}
-
-	if (this.y > p.y){
-	    return 1;
-	}
-
-	if (this.y < p.y){
-	    return -1;
-	}
-
-	return 0;
+  // Compare this Point to another Point p for the purposes of
+  // sorting a collection of points. The comparison is according to
+  // lexicographical ordering. That is, (x, y) < (x', y') if (1) x <
+  // x' or (2) x == x' and y < y'.
+  this.compareTo = function (p) {
+    if (this.x > p.x) {
+      return 1;
     }
 
-    // return a string representation of this Point
-    this.toString = function (){
-	return "(" + x + ", " + y + ")";
+    if (this.x < p.x) {
+      return -1;
     }
+
+    if (this.y > p.y) {
+      return 1;
+    }
+
+    if (this.y < p.y) {
+      return -1;
+    }
+
+    return 0;
+  };
+
+  // return a string representation of this Point
+  this.toString = function () {
+    return "(" + x + ", " + y + ")";
+  };
 }
 
 // An object that represents a set of Points in the plane. The `sort`
@@ -47,186 +53,267 @@ function Point (x, y, id){
 // points. The functions getXCoords and getYCoords return arrays
 // containing x-coordinates and y-coordinates (respectively) of the
 // points in the PointSet.
-function PointSet (){
-    this.points = [];
-    this.curPointID = 0;
+function PointSet() {
+  this.points = [];
+  this.curPointID = 0;
+  
+  // create a new Point with coordintes (x, y) and add it to this
+  // PointSet
+  this.addNewPoint = function (x, y) {
+    const pt = new Point(x,y, this.curPointID);
+    this.points.push(pt);
+    this.curPointID++;
+    return pt;
+  };
 
-    // create a new Point with coordintes (x, y) and add it to this
-    // PointSet
-    this.addNewPoint = function (x, y){
-	this.points.push(new Point(x, y, this.curPointID));
-	this.curPointID++;
+  // add an existing point to this PointSet
+  this.addPoint = function (pt) {
+    this.points.push(pt);
+  };
+
+  // sort the points in this.points
+  this.sort = function () {
+    this.points.sort((a, b) => {
+      return a.compareTo(b);
+    });
+  };
+
+  // reverse the order of the points in this.points
+  this.reverse = function () {
+    this.points.reverse();
+  };
+
+  // return an array of the x-coordinates of points in this.points
+  this.getXCoords = function () {
+    let coords = [];
+    for (let pt of this.points) {
+      coords.push(pt.x);
     }
 
-    // add an existing point to this PointSet
-    this.addPoint = function (pt){
-	this.points.push(pt);
+    return coords;
+  };
+
+  // return an array of the y-coordinates of points in this.points
+  this.getYCoords = function () {
+    let coords = [];
+    for (pt of this.points) {
+      coords.push(pt.y);
     }
 
-    // sort the points in this.points 
-    this.sort = function (){
-	this.points.sort((a,b) =>{return a.compareTo(b)});
+    return coords;
+  };
+
+  // get the number of points
+  this.size = function () {
+    return this.points.length;
+  };
+
+  // return a string representation of this PointSet
+  this.toString = function () {
+    let str = "[";
+    for (let pt of this.points) {
+      str += pt + ", ";
     }
+    str = str.slice(0, -2); // remove the trailing ', '
+    str += "]";
 
-    // reverse the order of the points in this.points
-    this.reverse = function (){
-	this.points.reverse();
-    }
+    return str;
+  };
 
-    // return an array of the x-coordinates of points in this.points
-    this.getXCoords = function (){
-	let coords = [];
-	for (let pt of this.points){
-	    coords.push(pt.x);
-	}
-
-	return coords;
-    }
-
-    // return an array of the y-coordinates of points in this.points
-    this.getYCoords = function (){
-	let coords = [];
-	for (pt of this.points){
-	    coords.push(pt.y);
-	}
-
-	return coords;
-    }
-
-    // get the number of points 
-    this.size = function (){
-	return this.points.length;
-    }
-
-    // return a string representation of this PointSet
-    this.toString = function (){
-	let str = '[';
-	for (let pt of this.points){
-	    str += pt + ', ';
-	}
-	str = str.slice(0,-2); 	// remove the trailing ', '
-	str += ']';
-
-	return str;
-    }
+ 
 }
 
+function ConvexHullViewer(svg, ps) {
+  this.svg = svg; // a n svg object where the visualization is drawn
+  this.ps = ps; // a point set of the points to be visualized
+  this.lines = [];
+  this.dots = [];
+  this.nextLineID = 0;
 
-// function ConvexHullViewer (svg, ps){
-//     this.svg = svg;  // a n svg object where the visualization is drawn
-//     this.ps = ps;    // a point set of the points to be visualized
+  this.svg.addEventListener("click", (e) => this.drawDot(e)); //e is a parameter for an event object. e=> makes a reference to an event object
+  //(i.e. this e in the function(e))
+ 
+  this.drawDot = function (e) {
+    let rect = this.svg.getBoundingClientRect(); //box is svg element and this is coordinated in the box
+    const x = e.clientX - rect.left; // coordinated relative to body - coordinates relative to webpage = exactly where mouse clicked
+    const y = e.clientY - rect.top;
+    let dot = document.createElementNS(SVG_NS, "circle"); //create element
+    dot.setAttributeNS(null, "cx", x); //attributes of location
+    dot.setAttributeNS(null, "cy", y);
+    this.ps.addNewPoint(x, y); //add the points to the pointset
+    this.dots.push(dot);
+    dot.classList.add("dot");
+    this.svg.appendChild(dot);
+  };
 
-//     // COMPLETE THIS OBJECT
-// }
+  this.clickDot = function (dot) {
+    console.log("You clicked this dot " + dot.id);
+  }
 
-function ConvexHullViewer (svg, ps){
-    this.svg = svg;  // an SVG object where the visualization is drawn
-    this.ps = ps;    // a PointSet of the points to be visualized
+  this.addLine = function (line) {
+    const a = line.a;
+    const b = line.b;
+    const draw = document.createElementNS(SVG_NS, "line");
+    draw.setAttributeNS(null, "x1", a.x); //set its points
+    draw.setAttributeNS(null, "y1", a.y);
+    draw.setAttributeNS(null, "x2", b.x);
+    draw.setAttributeNS(null, "y2", b.y);
+    draw.classList.add("line");
+    line.id = this.nextLineID;
+    this.nextLineID++;
+    this.lines[line.id] = line;
+    this.svg.appendChild(line);
+  }
 
-    // COMPLETE THIS OBJECT
+  this.removeLine = function(line) {
+    let rem = this.lines[line.id];
+    rem.remove();
+  }
+
+}
+
+function Line(a,b,id){
+  this.a = a;
+  this.b = b;
+  this.id = id;
+
+
+  this.equals = function(a,b) {
+    return (this.a == this.a && this.b == b) || (this.a == b && this.b == a);
+  };
 }
 
 
 /*
- * An object representing an instance of the convex hull problem. A ConvexHull stores a PointSet ps that stores the input points, and a ConvexHullViewer viewer that displays interactions between the ConvexHull computation and the 
+ * An object representing an instance of the convex hull problem. A ConvexHull stores a PointSet ps that stores the input points, and a ConvexHullViewer viewer that displays interactions between the ConvexHull computation and the
  */
-function ConvexHull (ps, viewer){
-    this.ps = ps;          // a PointSet storing the input to the algorithm
-    this.viewer = viewer;  // a ConvexHullViewer for this visualization
+function ConvexHull(ps, viewer) {
+  this.ps = ps; // a PointSet storing the input to the algorithm
+  this.viewer = viewer; // a ConvexHullViewer for this visualization
+  // start a visualization of the Graham scan algorithm performed on ps
+  this.isLine = function(a,b) {
+    return (this.getLine(a,b) != null);
+  }
 
-    let s = [];
-    ps.sort();  // sort the points in ps
-    let a = ps.points[0];  // the point with the smallest x-coordinate
-    let b = ps.points[1];  // the second point with the smallest x-coordinate
-    let c = ps.points[2];  // the third point with the smallest x-coordinate
+  this.getLine = function(a,b) {
+    for(const line of this.lines) {
+      if(line.equals(a,b)) {
+        return line;
+      }
+    }
+  }
 
+  this.makeLine = function(a, b) {
+    if (!this.isLine(a, b)) {
+      const line = new Line(a, b, this.nextEdgeID);
+      this.nextEdgeID++;
+      for(let i = 0; i < this.dots.length; i++){
+        if (this.dots.points[i] = a) {
+          this.dots.points[i+1] = b;
+        } 
+      }
+      this.lines.push(line);
+      console.log("added line (" + a.id + ", " + b.id + ")");
+      return line;
+    } else {
+      console.log("line (" + a.id + ", " + b.id + ") not added because it is already in the graph");
+      return null;
+    }
+  }
 
-    // start a visualization of the Graham scan algorithm performed on ps
-    this.start = function (){
-        //COMPLETE THIS
-        this.step();
+  this.start = function () {
+    this.stack = [];
+    this.a = ps.points[0];
+    this.b = ps.points[1];
+    this.c = ps.points[2];
+    console.log("Click to start");
+
+    ch = this.getConvexHull;
+
+    for(let i = 0; i < ch.length -1 ; i++){
+      this.addLine(ch.points[i], ch.points[i+1]);
+    }
+    
+  }
+  // perform a single step of the Graham scan algorithm performed on ps
+  this.step = function () {
+    if (this.stack.length == 0) {
+      this.stack.push(this.a);
+      this.stack.push(this.b);
+      this.viewer.addLine(this.a, this.b);
+    } else if (!cross(this.a, this.b, this.c) && ps.size() > 1) {
+      this.stack.pop();
+      this.b = this.a;
+      this.a = this.stack[this.stack.length - 2];
+    } else {
+      this.stack.push(this.c);
+      this.viewer.drawLine(this.b, this.c);
+      this.a = this.b;
+      this.b = this.c;
+    }
+  };
+
+  // Return a new PointSet consisting of the points along the convex
+  // hull of ps. This method should **not** perform any
+  // visualization. It should **only** return the convex hull of ps
+  // represented as a (new) PointSet. Specifically, the elements in
+  // the returned PointSet should be the vertices of the convex hull
+  // in clockwise order, starting from the left-most point, breaking
+  // ties by minimum y-value.
+  this.getConvexHull = function () {
+    // Make a copy of the input points and sort them lexicographically.
+    let sortedPoints = this.ps.points.slice();
+    sortedPoints.sort((a, b) => {
+        if (a.x !== b.x) {
+            return a.x - b.x;
+        } else {
+            return a.y - b.y;
+        }
+    });
+
+    // Find the lower hull.
+    let lowerHull = [];
+    for (let i = 0; i < sortedPoints.length; i++) {
+        while (lowerHull.length >= 2 &&
+               cross(lowerHull[lowerHull.length - 2], lowerHull[lowerHull.length - 1], sortedPoints[i]) <= 0) {
+            lowerHull.pop();
+        }
+        lowerHull.push(sortedPoints[i]);
+        
     }
 
-    this.step = function (){
-        // get the size of the stack
-        let n = s.length;
-    
-        // get the two points at the top of the stack
-        let b = s[n-1];
-        let a = s[n-2];
-    
-        // get the next point in the PointSet
-        let c = ps.points[(ps.points.indexOf(b)+1) % ps.size()];
-    
-        // calculate the cross product of vectors (a,b) and (b,c)
-        let cp = cross(a,b,c);
-    
-        if (cp <= 0 && n > 2){
-            // if the cross product is negative or 0 and there are more
-            // than 2 points in the stack, pop the top point
-            s.pop();
-        } else{
-            // otherwise, push the next point onto the stack
-            s.push(c);
+    // Find the upper hull.
+    let upperHull = [];
+    for (let i = sortedPoints.length - 1; i >= 0; i--) {
+        while (upperHull.length >= 2 &&
+               cross(upperHull[upperHull.length - 2], upperHull[upperHull.length - 1], sortedPoints[i]) <= 0) {
+            upperHull.pop();
         }
-    
-        // update the visualization
-        viewer.update(s, ps);
+        upperHull.push(sortedPoints[i]);
     }
-    
-    this.getConvexHull = function (){
-        // Make a copy of the input points and sort them lexicographically.
-        let sortedPoints = this.ps.points.slice();
-        sortedPoints.sort((a, b) =>{
-            if (a.x !== b.x){
-                return a.x - b.x;
-            } else{
-                return a.y - b.y;
-            }
-        });
 
-        // Find the lower hull.
-        let lowerHull = [];
-        for (let i = 0; i < sortedPoints.length; i++){
-            while (lowerHull.length >= 2 &&
-                   cross(lowerHull[lowerHull.length - 2], lowerHull[lowerHull.length - 1], sortedPoints[i]) <= 0){
-                lowerHull.pop();
-            }
-            lowerHull.push(sortedPoints[i]);
-        }
+    // Remove the first and last points of the upper hull (they are also in the lower hull).
+    //upperHull.shift();
+    lowerHull.pop();
+    upperHull.reverse();
+    lowerHull.reverse();
 
-        // Find the upper hull.
-        let upperHull = [];
-        for (let i = sortedPoints.length - 1; i >= 0; i--){
-            while (upperHull.length >= 2 &&
-                   cross(upperHull[upperHull.length - 2], upperHull[upperHull.length - 1], sortedPoints[i]) <= 0){
-                upperHull.pop();
-            }
-            upperHull.push(sortedPoints[i]);
-        }
+    // Concatenate the lower hull and the upper hull to form the convex hull.
+    let convexHull = new PointSet();
+    convexHull.points = upperHull.concat(lowerHull);
 
-        // Remove the first and last points of the upper hull (they are also in the lower hull).
-        //upperHull.shift();
-        lowerHull.pop();
-        upperHull.reverse();
-        lowerHull.reverse();
-
-        // Concatenate the lower hull and the upper hull to form the convex hull.
-        let convexHull = new PointSet();
-        convexHull.points = upperHull.concat(lowerHull);
-
-        return convexHull;
-    }
+    return convexHull;
+  }
 
 }
 
 // Compute the cross product of the vectors (p1 -> p2) and (p2 -> p3).
-function cross(p1, p2, p3){
-    return (p2.x - p1.x) * (p3.y - p2.y) - (p3.x - p2.x) * (p2.y - p1.y);
+function cross(p1, p2, p3) {
+return (p2.x - p1.x) * (p3.y - p2.y) - (p3.x - p2.x) * (p2.y - p1.y);
 }
-try{
-    exports.PointSet = PointSet;
-    exports.ConvexHull = ConvexHull;
-  } catch (e){
-    console.log("not running in Node");
-  }
+try {
+exports.PointSet = PointSet;
+exports.ConvexHull = ConvexHull;
+} catch (e) {
+console.log("not running in Node");
+}
