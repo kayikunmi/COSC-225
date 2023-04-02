9250131d3f2fe0a778d60c0a8e2449b58a6011f0
KayikunmiMac <kayikunmiba.com>
2023-04-02 13:27:20 -0400

    add reqs

--- WK 09/HW07/script.js
+++ WK 09/HW07/script.js
@@ -1,30 +1,46 @@
-function drawCircle(x, y, r, depth) {
+function drawCircle(x, y, r, depth, scaleFactor) {
   if (depth <= 0) {
     return;
   }
 
-  // Add circle
   var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
   circle.setAttribute("cx", x);
   circle.setAttribute("cy", y);
   circle.setAttribute("r", r);
   circle.setAttribute("fill", "none");
   circle.setAttribute("stroke", "black");
+  circle.setAttribute("transform", "scale(" + scaleFactor + ")");
+
   var svg = document.getElementById("main-svg");
   svg.appendChild(circle);
 
-  // Add triangles
-  var triangle = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-  var points = [    (x - r) + "," + y,    x + "," + (y - r),    (x + r) + "," + y,    x + "," + (y + r)  ];
-  triangle.setAttribute("points", points.join(" "));
-  triangle.setAttribute("fill", "rgb(123,165,133)");
-  svg.appendChild(triangle);
-
-  // Recursively add smaller circles and triangles
-  drawCircle(x - r, y, r * 0.5, depth - 1);
-  drawCircle(x + r, y, r * 0.5, depth - 1);
-  drawCircle(x, y - r, r * 0.5, depth - 1);
-  drawCircle(x, y + r, r * 0.5, depth - 1);
+  drawCircle(x - r, y, r * 0.5, depth - 1, scaleFactor);
+  drawCircle(x + r, y, r * 0.5, depth - 1, scaleFactor);
+  drawCircle(x, y - r, r * 0.5, depth - 1, scaleFactor);
+  drawCircle(x, y + r, r * 0.5, depth - 1, scaleFactor);
 }
 
-drawCircle(325, 325, 150, 4);
+function drawTriangle(x, y, r, depth, scaleFactor) {
+  if (depth <= 0) {
+    return;
+  }
+
+  var triangle = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+  var points = (x-r) + "," + y + " " + x + "," + (y-r) + " " + (x+r) + "," + y;
+  triangle.setAttribute("points", points);
+  triangle.setAttribute("fill", "rgb(123,165,133)");
+  triangle.setAttribute("stroke", "none");
+  triangle.setAttribute("transform", "scale(" + scaleFactor + ")");
+
+  var svg = document.getElementById("main-svg");
+  svg.appendChild(triangle);
+
+  drawTriangle(x - r, y, r * 0.5, depth - 1, scaleFactor);
+  drawTriangle(x + r, y, r * 0.5, depth - 1, scaleFactor);
+  drawTriangle(x, y - r, r * 0.5, depth - 1, scaleFactor);
+  drawTriangle(x, y + r, r * 0.5, depth - 1, scaleFactor);
+}
+
+var scaleFactor = 0.5;
+drawCircle(600, 600, 300, 4, scaleFactor);
+drawTriangle(600, 600, 300, 4, scaleFactor);
