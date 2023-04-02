9686caadf6daece6f258de09171d9c6ccce7c42a
KayikunmiMac <kayikunmiba.com>
2023-04-02 13:13:03 -0400

    perf

--- WK 09/HW07/script.js
+++ WK 09/HW07/script.js
@@ -3,21 +3,28 @@ function drawCircle(x, y, r, depth) {
     return;
   }
 
+  // Add circle
   var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
   circle.setAttribute("cx", x);
   circle.setAttribute("cy", y);
   circle.setAttribute("r", r);
   circle.setAttribute("fill", "none");
   circle.setAttribute("stroke", "black");
-  circle.setAttribute("transform", "rotate(" + (x + y) + " " + x + " " + y + ")");
-
   var svg = document.getElementById("main-svg");
   svg.appendChild(circle);
 
+  // Add triangles
+  var triangle = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+  var points = [    (x - r) + "," + y,    x + "," + (y - r),    (x + r) + "," + y,    x + "," + (y + r)  ];
+  triangle.setAttribute("points", points.join(" "));
+  triangle.setAttribute("fill", "rgb(123,165,133)");
+  svg.appendChild(triangle);
+
+  // Recursively add smaller circles and triangles
   drawCircle(x - r, y, r * 0.5, depth - 1);
   drawCircle(x + r, y, r * 0.5, depth - 1);
   drawCircle(x, y - r, r * 0.5, depth - 1);
   drawCircle(x, y + r, r * 0.5, depth - 1);
 }
 
-drawCircle(325,325, 150, 4);
+drawCircle(325, 325, 150, 4);
