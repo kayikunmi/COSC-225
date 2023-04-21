b9dd70de73e776ca22eef506bbc2335a748451e0
KayikunmiMac <kayikunmiba.com>
2023-04-21 10:09:17 -0400

    fixed 4

--- WK 04/HW4/cellular-automata.js
+++ WK 04/HW4/cellular-automata.js
@@ -21,43 +21,45 @@ function applyRule(config, rule){
   //console.log("nextConfig = " + nextConfig);
   return nextConfig;
 }
-/**/
-let form = document.querySelector('form');
-let grid = document.querySelector('#grid');
-  
-form.addEventListener('submit', (event) => {
-  //prevent the default form submission behavior
-  event.preventDefault();
-  let ruleInput = document.querySelector('#rule');
-  let rule = ruleInput.value;
 
-  //clear the grid before generating new cellular automaton
-  grid.innerHTML = '';
-  
-  let numRows = 150;
-  let numCols = 150;
-  
-  //initialize the configuration with a single cell in the middle
-  let config = new Array(numCols).fill(0);
-  config[0]=1;
-  config[numCols-1] =1;
-  config[Math.floor(numCols / 2)] = 1;
-  
-  //generate the cellular automaton by looping thrugh each cell
-  for (let i = 0; i < numRows; i++) {
-    const row = document.createElement('tr');
-    for (let j = 0; j < numCols; j++) {
-      const cell = document.createElement('td');
-      //change the class to alive when the number is 1. 
-      //this will change the color to whatevr alive's css is
-      cell.classList.toggle('alive', config[j] === 1);
-      row.appendChild(cell);
+let form, grid;
+if (typeof document !== 'undefined') {
+  form = document.querySelector('form');
+  grid = document.querySelector('#grid');
+
+  form.addEventListener('submit', (event) => {
+    //prevent the default form submission behavior
+    event.preventDefault();
+    let ruleInput = document.querySelector('#rule');
+    let rule = ruleInput.value;
+
+    //clear the grid before generating new cellular automaton
+    grid.innerHTML = '';
+
+    let numRows = 150;
+    let numCols = 150;
+
+    //initialize the configuration with a single cell in the middle
+    let config = new Array(numCols).fill(0);
+    config[0]=1;
+    config[numCols-1] =1;
+    config[Math.floor(numCols / 2)] = 1;
+
+    //generate the cellular automaton by looping through each cell
+    for (let i = 0; i < numRows; i++) {
+      const row = document.createElement('tr');
+      for (let j = 0; j < numCols; j++) {
+        const cell = document.createElement('td');
+        //change the class to alive when the number is 1. 
+        //this will change the color to whatever alive's css is
+        cell.classList.toggle('alive', config[j] === 1);
+        row.appendChild(cell);
+      }
+
+      config = applyRule(config, rule);
+      grid.appendChild(row);
     }
-  
-    config = applyRule(config, rule);
-    grid.appendChild(row);
-  }
-});
+  });
+}
 
 module.exports = { applyRule };
-  
